4456e2259ce1cde6239b95a364f702d56c2c5350
kelvintsim <83445753+kelvintsim@users.noreply.github.com>
2023-07-18 16:00:04 +0800

    adjust promotion prompt

--- promotion_generator/handler.py
+++ promotion_generator/handler.py
@@ -18,7 +18,7 @@ def ask_name(start_date: str, end_date: str, start_time: str, end_time: str, day
         "api-key": Config.AZURE_OPENAI_KEY
     }
     user_prompt = (
-        "Generate a promotion 'traditional chinese' event name for a restaurant base on the following period of date range, time range and days: \n"
+        "Generate a promotion 'traditional chinese' event name with 10 words for a restaurant base on the following period of date range, time range and days: \n"
         f"""
         date_range: {start_date} - {end_date}
         time_range: {start_time} - {end_time}
@@ -26,17 +26,17 @@ def ask_name(start_date: str, end_date: str, start_time: str, end_time: str, day
         """
     )
     sample_response = (
-        "中華美食工作日盛宴"
-        "尊享中式午茶時光"
-        "黄金蝦仁周中宴"
-        "節日美饌周日珍宴"
-        "聖夜之星美食之旅"
+        "中華美食工作日盛宴, "
+        "尊享中式午茶時光, "
+        "黄金蝦仁周中宴, "
+        "節日美饌周日珍宴, "
+        "聖夜之星美食之旅, "
     )
 
     chatgpt_data = {
         "messages": [
             {"role": "user", "content": user_prompt},
-            {"role": "user", "content": sample_response},
+            {"role": "assistant", "content": sample_response},
         ],
         "max_tokens": 800,
         "temperature": 0.2,
