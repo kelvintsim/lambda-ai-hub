1f012d7cdf4f5bc7085271a98a69025f88ce9a72
kelvintsim <83445753+kelvintsim@users.noreply.github.com>
2023-07-27 11:08:41 +0800

    add image_generator log

--- services/foodimage_generator/handler.py
+++ services/foodimage_generator/handler.py
@@ -16,8 +16,9 @@ get_n8n_url = Config.GET_N8N_ENDPOINT
 def get_id(prompt: str) -> str:
     gen_photo_url = urljoin(gen_n8n_url, prompt)
     response = requests.post(gen_photo_url, auth=(account, password), json={})
+    print(response.json())
     task = response.json()
-    task_id = task["taskId"]
+    task_id = task[0]["taskId"]
     return task_id
 
 def genphoto_handler(event: EventDict, context) -> dict[str, str]:
