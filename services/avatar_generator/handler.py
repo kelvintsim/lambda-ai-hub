57bcd4c30988ee42452d67bc0375cf04a4bc1859
Xavier Au <xavierau@yoov.com>
2023-09-25 16:52:43 +0800

    update rowId for gen avatar

--- services/avatar_generator/handler.py
+++ services/avatar_generator/handler.py
@@ -4,21 +4,30 @@ import datetime
 from requests.compat import urljoin
 from config import Config
 
+
 class EventDict(TypedDict):
     type: str
     name: str
 
+
 account = Config.MIDJOURNEY_ACCOUNT
 password = Config.MIDJOURNEY_PASSWORD
 
-def get_id(photo: str) -> str:
+
+def get_id(photo: str, rowId: str) -> str:
     gen_n8n_url = Config.AVATAR_N8N_ENDPOINT
-    response = requests.post(gen_n8n_url, auth=(account, password), json={"prompt": f"{photo} Generate a cartoon style avatar to replace only the face of the person, please keep the original background, try to make the cartoon avatar as much alike to the person."})
+    data = {
+        "prompt": f"{photo} Generate a cartoon style avatar to replace only the face of the person, please keep the original background, try to make the cartoon avatar as much alike to the person.",
+        "rowId": rowId
+    }
+    response = requests.post(gen_n8n_url, auth=(account, password), json=data)
     task = response.json()
     task_id = task["taskId"]
     return task_id
 
+
 def genphoto_handler(event: EventDict, context) -> dict[str, str]:
     prompt = event["body"]["prompt"]
-    task_id = get_id(prompt)
-    return task_id
+    rowId = event["body"]["rowId"]
+    task_id = get_id(prompt, rowId)
+    return {"task_id": task_id}
